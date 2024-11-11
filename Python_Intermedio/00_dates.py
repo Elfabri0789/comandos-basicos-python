b14e5ef10b1e2c47058257436249d235a24a9d95
elfabri07 <elfabri0789@gmail.com>
2024-11-11 17:45:49 -0300

    agregamos nuevo fichero

--- Python_Intermedio/00_dates.py
+++ Python_Intermedio/00_dates.py
@@ -16,3 +16,30 @@ timestamp= ahora.timestamp() #TIMESTAMP ES UNA PALABRA RESERVADA Y SIGNIFICA EN 
 print(timestamp)
 
 year_2025= datetime(2025,1,1)
+
+
+def print_date(date):
+    print(date.year)
+    print(date.month)
+    print(date.day)
+    print(date.hour)
+    print(date.minute)
+    print(date.second)
+
+print_date(ahora)
+print_date(year_2025)
+
+from datetime import date    #ACCEDEMOS A MODULO#
+
+current_time= date.today()
+
+print(current_time.year)
+print(current_time.month)
+print(current_time.day)
+
+diff= year_2025 - ahora  #hacemos la funcion para saber cuanto queda hasta el nuevo año
+print(diff)
+
+from datetime import timedelta    #ACCEDEMOS A MODULO#
+
+time_delta= timedelta()
